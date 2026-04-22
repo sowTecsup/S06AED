f70695667c48eb98a49bcee6045a2641c86887db
sowTecsup <jneyra@tecsup.edu.pe>
2026-04-22 11:07:55 -0500

    Update WindowManager.cs

--- Assets/Scripts/WindowsExcercise/WindowManager.cs
+++ Assets/Scripts/WindowsExcercise/WindowManager.cs
@@ -19,7 +19,8 @@ public class WindowManager : MyStack<Window>
 
     public override Window Pop()//+1 jeremy , coreting , arribasplata
     {
-         OnElementRemoved?.Invoke(Peek());
+        //->si el elemento que popeo es esta activo tiro el OnElementRemoved , caso contrario no lo realizo.    (IF)
+        OnElementRemoved?.Invoke(Peek());
          return base.Pop();
     }
 
