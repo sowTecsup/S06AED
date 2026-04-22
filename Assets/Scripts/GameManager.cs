63355f5a497af7deb70a965d664f89f6be925969
sowTecsup <jneyra@tecsup.edu.pe>
2026-04-22 09:18:28 -0500

    Stack Update

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+     void Start()
     {
         
     }
---
+++ Assets/Scripts/Stacks/MyStack.cs
@@ -0,0 +1,64 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MyStack<T> //->strings
+{
+
+    #region Privates
+    private StackNode<T> top;
+    private int count;
+    #endregion
+    #region Public Methods
+    public void Push(T value)
+    {
+        StackNode<T> newNode = new StackNode<T>(value);
+        //->pila vacia
+        if(top == null)
+        {
+            top = newNode;
+            return;
+        }
+        //->pila ya tiene un elemtno
+        newNode.SetNext(top);
+        top = newNode;
+
+        count++;    
+    }
+
+    public T Pop()
+    {
+        if (top == null) 
+            throw new System.Exception("Trying to pop a empty stack");
+
+        T tempValue = top.Value;
+      // StackNode<T> tempValue = top;
+
+        top = top.Next;
+        count--;
+
+        return tempValue;
+    }
+    public T Peek()//-> +1  Jeremy , arribasplata, traviz 
+    {
+        if (top == null)
+            throw new System.Exception("Trying to peek a empty stack");
+
+        T tempValue = top.Value;
+        return tempValue;
+    }
+
+
+    public void Clear()//+1  coretin, juan diego, guerrero erwin !
+    {
+        top = null;
+        count = 0;
+    }
+    #endregion
+    #region Getters
+    public StackNode<T> Top => top;
+    public int Count => count;
+    #endregion
+
+
+
+}
